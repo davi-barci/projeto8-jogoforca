4ea007294a208d342424be7b42897e9a33438cfa
Davi Barci <davibarci0208@gmail.com>
2023-02-20 23:59:13 -0300

    Componentização inicial do projeto

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Jogo from './components/Jogo';
+import Letras from './components/Letras';
+
+export default function App() {
+  return (
+    <div>
+      Projeto Forca
+    </div>
+  );
+}
+
+
